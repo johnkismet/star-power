f8f6107b07792b54bf7673d2ebe7c45b03cf2319
johnkismet <mojomanftw@gmail.com>
2021-03-21 10:12:52 -0400

    Able to respond to mentions

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const { WebClient } = require("@slack/web-api");
+const { createEventAdapter } = require("@slack/events-api");
+const port = 3000;
+
+const slackEvents = createEventAdapter(process.env.SIGNING_SECRET);
+const slackClient = new WebClient(process.env.SLACK_TOKEN);
+
+slackEvents.on("app_mention", (event) => {
+	console.log(`Got message from user ${event.user}: ${event.text}`);
+	(async () => {
+		try {
+			await slackClient.chat.postMessage({
+				channel: event.channel,
+				text: `Hello <@${event.user}>! :tada:`,
+			});
+		} catch (error) {
+			console.log(error.data);
+		}
+	})();
+});
+
+slackEvents.on("error", console.error);
+
+slackEvents.start(port).then(() => {
+	console.log(`Server started on port ${port}!`);
+});
--- src/app.js
+++
@@ -1,6 +0,0 @@
-import { SIGNING_SECRET } from "./constants";
-
-const { WebClient } = require("@slack/web-api");
-const { createEventAdapter } = require("@slack/events-api");
-
-const slackEvents = createEventAdapter(SIGNING_SECRET);
